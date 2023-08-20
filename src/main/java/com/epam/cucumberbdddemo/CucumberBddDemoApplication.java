d5eb170d994057e19fb051ef7e20a60c3f4906a6
MohammadOsama Khan <mohammadosama_khan@epam.com>
2023-08-20 22:42:39 +0530

    added all files

---
+++ src/main/java/com/epam/cucumberbdddemo/CucumberBddDemoApplication.java
@@ -0,0 +1,13 @@
+package com.epam.cucumberbdddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberBddDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CucumberBddDemoApplication.class, args);
+    }
+
+}
